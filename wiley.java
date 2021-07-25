292feaccfbca1d901c87028e70520c6986b82e59
Jagmeet <jagmeetsingh89.st@gmail.com>
2021-07-25 14:38:41 +0530

    abc

--- wiley.java
+++ wiley.java
@@ -3,7 +3,5 @@
 class wiley {
     public static void main(String[] args) {
         System.out.println("Sum of two wiley");
-        System.out.println("abc.");
-        System.out.println("This is first program in Java.");
     }
 }
